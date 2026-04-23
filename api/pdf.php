fa13833307731991fccfcf1b8df014dceeaf2d0d
optmsshiv <optmsshiv@gmail.com>
2026-04-24 01:50:22 +0530

    error detection added

--- api/pdf.php
+++ api/pdf.php
@@ -10,7 +10,9 @@
 // ================================================================
 
 ob_start();
-error_reporting(0);
+// error_reporting(0);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 require_once __DIR__ . '/../config/db.php';
 
